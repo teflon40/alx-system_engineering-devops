48c9032eee04bd9c7c57aba678dfec4f526c571e
teflon40 <frimpong.bernard3220@gmail.com>
2024-06-28 15:11:23 -0700

    Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# creates a file in /tmp with the below attributes
+file {'/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
